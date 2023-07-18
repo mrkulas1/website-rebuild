063e827c913ad63b5716db79bea6a4ee36a502b5
mrkulas1 <98844790+mrkulas1@users.noreply.github.com>
2023-07-18 11:17:56 -0700

    Created 3 new PHP files for reducing complexity

---
+++ menu.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>HFHMR</title>
+</head>
+<link href="style.css" rel="stylesheet">
+<body>
+    <div class="navbar">
+      <a href="index.html">Home</a>
+        <div class="dropdown">
+            <button class="dropbtn">
+              <i class="fa fa-caret-down"></i>
+              <a href="about.html">About Us</a>
+            </button>
+            <!--<a href="#about">About Us
+              <i class="fa fa-caret-down"></i>
+            </a>-->
+            <div class="dropdown-content">
+              <a href="where_we_build.html">Where We Build</a>
+              <a href="current_builds.html">Current Builds</a>
+              <a href="house_photos.html">House Photos</a>
+              <a href="corporate_sponsors.html">Corporate Sponsors</a>
+              <a href="newsletter_signup.html">Newsletter Sign Up</a>
+              <a href="newsletter.html">Newsletters</a>
+              <a href="press_release.html">Press Releases</a>
+            </div>
+          </div>
+        <a href="contact.html">Contact Us</a>
+        <div class="dropdown">
+            <button class="dropbtn"> 
+              <i class="fa fa-caret-down"></i>
+              <a href="our_services.html">What We Do</a>
+            </button>
+            <div class="dropdown-content">
+              <a href="home_ownership.html">Home Ownership</a>
+              <a href="repair_program.html">Owner Occupied Repair Loans</a>
+            </div>
+          </div> 
+          <div class="dropdown">
+            <button class="dropbtn"> 
+              <i class="fa fa-caret-down"></i>
+              <a href="get_involved.html">Get Involved</a>
+            </button>
+            <div class="dropdown-content">
+              <a href="core_crew.html">Core Crew</a>
+              <a href="events.html">Events</a>
+              <a href="committees.html">Committees</a>
+              <a href="group_builds.html">Group Builds</a>
+              <a href="youth_programs.html">Youth Programs</a>
+            </div>
+          </div>
+          <div class="dropdown">
+            <button class="dropbtn"> 
+              <i class="fa fa-caret-down"></i>
+              <a href="donate.html">Donate</a>
+            </button>
+            <div class="dropdown-content">
+              <a href="sponsor.html">Sponsor An Event</a>
+              <a href="other_donations.html">Other Ways to Donate</a>
+              <a href="donate_faq.html">Donation FAQ</a>
+              <a href="thrivent.html">Thrivent</a>
+              <a href="cars.html">Cars for Homes</a>
+            </div>
+          </div>
+          <div class="dropdown">
+            <button class="dropbtn"> 
+              <i class="fa fa-caret-down"></i>
+              <a href="restore.html">Our ReStore</a>
+            </button>
+            <div class="dropdown-content">
+              <a href="hours.html">Hours and Location</a>
+              <a href="shop.html">Shop</a>
+              <a href="donate_goods.html">Donate</a>
+              <a href="volunteer.html">Volunteer</a>
+              <a href="restore_faq.html">ReStore FAQ</a>
+            </div>
+          </div>
+      </div>
