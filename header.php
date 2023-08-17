d993f6db5612e97388b415b25eb231bc4da393b7
mrkulas1 <98844790+mrkulas1@users.noreply.github.com>
2023-08-17 10:25:49 -0700

    PHP abstraction for menu and header
    Added the final menu to the php abstraction.  Also modified the look of the footer for simplicity and ease of use

--- header.php
+++ header.php
@@ -1,9 +1,13 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>HFHMR</title>
-</head>
-<link href="style.css" rel="stylesheet">
-<body>
+
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>HFH Menominee River</title>
+  </head>
+  <link href="style.css" rel="stylesheet">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+
+  <body>
--- menu.php
+++ menu.php
@@ -1,68 +1,94 @@
-    <div class="navbar">
-      <a href="index.html">Home</a>
-        <div class="dropdown">
-            <button class="dropbtn">
-              <i class="fa fa-caret-down"></i>
-              <a href="about.html">About Us</a>
-            </button>
-            <div class="dropdown-content">
-              <a href="where_we_build.html">Where We Build</a>
-              <a href="current_builds.html">Current Builds</a>
-              <a href="house_photos.html">House Photos</a>
-              <a href="corporate_sponsors.html">Corporate Sponsors</a>
-              <a href="newsletter_signup.html">Newsletter Sign Up</a>
-              <a href="newsletter.html">Newsletters</a>
-              <a href="press_release.html">Press Releases</a>
-            </div>
-          </div>
-        <a href="contact.html">Contact Us</a>
-        <div class="dropdown">
-            <button class="dropbtn"> 
-              <i class="fa fa-caret-down"></i>
-              <a href="our_services.html">What We Do</a>
-            </button>
-            <div class="dropdown-content">
-              <a href="home_ownership.html">Home Ownership</a>
-              <a href="repair_program.html">Owner Occupied Repair Loans</a>
-            </div>
-          </div> 
-          <div class="dropdown">
-            <button class="dropbtn"> 
-              <i class="fa fa-caret-down"></i>
-              <a href="get_involved.html">Get Involved</a>
-            </button>
-            <div class="dropdown-content">
-              <a href="core_crew.html">Core Crew</a>
-              <a href="events.html">Events</a>
-              <a href="committees.html">Committees</a>
-              <a href="group_builds.html">Group Builds</a>
-              <a href="youth_programs.html">Youth Programs</a>
-            </div>
-          </div>
-          <div class="dropdown">
-            <button class="dropbtn"> 
-              <i class="fa fa-caret-down"></i>
-              <a href="donate.html">Donate</a>
-            </button>
-            <div class="dropdown-content">
-              <a href="sponsor.html">Sponsor An Event</a>
-              <a href="other_donations.html">Other Ways to Donate</a>
-              <a href="donate_faq.html">Donation FAQ</a>
-              <a href="thrivent.html">Thrivent</a>
-              <a href="cars.html">Cars for Homes</a>
-            </div>
-          </div>
-          <div class="dropdown">
-            <button class="dropbtn"> 
-              <i class="fa fa-caret-down"></i>
-              <a href="restore.html">Our ReStore</a>
-            </button>
-            <div class="dropdown-content">
-              <a href="hours.html">Hours and Location</a>
-              <a href="shop.html">Shop</a>
-              <a href="donate_goods.html">Donate</a>
-              <a href="volunteer.html">Volunteer</a>
-              <a href="restore_faq.html">ReStore FAQ</a>
-            </div>
-          </div>
+<div class="header sticky" id="myHeader">
+    <div class="banner" id="myBanner">
+      <div class="flex-child">
+          <a href="index.html">
+            <img src="New Logo.JPG" alt="Habitat for Humanity Menominee River Logo" style="width:50%;">
+          </a>
       </div>
+      <div class="flex-child">
+        <div class="media-links">
+          <a href="https://www.facebook.com/habitat.river/" target="_blank" class="fa fa-facebook"></a>
+          <a href="https://www.instagram.com" target="_blank" class="fa fa-instagram"></a>
+          <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" target="_blank" class="fa fa-youtube"></a>
+        </div>
+      </div>
+    </div>
+    <div class="navbar" id="mynavbar">
+      <div class="dropdown">
+        <button class="dropbtn">
+          <a href="about.html">About Us</a>
+        </button>
+        <div class="dropdown-content">
+          <a href="where_we_build.html">Where We Build</a>
+          <a href="current_builds.html">Current Builds</a>
+          <a href="house_photos.html">House Photos</a>
+          <a href="corporate_sponsors.html">Corporate Sponsors</a>
+          <a href="newsletter_signup.html">Newsletter Sign Up</a>
+          <a href="newsletter.html">Newsletters</a>
+          <a href="press_release.html">Press Releases</a>
+        </div>
+      </div>
+      <div class="dropdown">
+        <button class="dropbtn">
+          <a href="contact.html">Contact Us</a>
+        </button>
+      </div>
+      <div class="dropdown">
+        <button class="dropbtn">
+          <a href="our_services.html">What We Do</a>
+        </button>
+        <div class="dropdown-content">
+          <a href="home_ownership.html">Home Ownership</a>
+          <a href="repair_program.html">Owner Occupied Repair Loans</a>
+        </div>
+      </div>
+      <div class="dropdown">
+        <button class="dropbtn">
+          <a href="get_involved.html">Get Involved</a>
+        </button>
+        <div class="dropdown-content">
+          <a href="core_crew.html">Core Crew</a>
+          <a href="events.html">Events</a>
+          <a href="committees.html">Committees</a>
+          <a href="group_builds.html">Group Builds</a>
+          <a href="youth_programs.html">Youth Programs</a>
+        </div>
+      </div>
+      <div class="dropdown">
+        <button class="dropbtn">
+          <a href="donate.html">Donate</a>
+        </button>
+        <div class="dropdown-content">
+          <a href="sponsor.html">Sponsor An Event</a>
+          <a href="other_donations.html">Other Ways to Donate</a>
+          <a href="donate_faq.html">Donation FAQ</a>
+          <a href="thrivent.html">Thrivent</a>
+          <a href="cars.html">Cars for Homes</a>
+        </div>
+      </div>
+      <div class="dropdown">
+        <button class="dropbtn">
+          <a href="restore.html">Our ReStore</a>
+        </button>
+        <div class="dropdown-content">
+          <a href="hours.html">Hours and Location</a>
+          <a href="shop.html">Shop</a>
+          <a href="donate_goods.html">Donate</a>
+          <a href="volunteer.html">Volunteer</a>
+          <a href="restore_faq.html">ReStore FAQ</a>
+        </div>
+      </div>
+      <div class="search-container" id="mySearch">
+        <form action="action_page.php">
+          <input type="text" placeholder="Search.." name="search">
+          <button type="submit"><i class="fa fa-search"></i></button>
+        </form>
+      </div>
+      <a href="javascript:void(0);" class="icon" onclick="openMenu()">
+        <i class="fa fa-bars"></i>
+      </a>
+      <a href="javascript:void(0);" class="search-icon" onclick="openSearch()">
+        <i class="fa fa-search"></i>
+      </a>
+    </div>
+  </div>
