ede6c4bf1e12b4149a57bbc4c28b4c970c9eaead
mrkulas1 <98844790+mrkulas1@users.noreply.github.com>
2023-08-28 16:30:04 -0700

    Added a restore page
    Working on layout for restore page

--- contact.php
+++ contact.php
@@ -4,15 +4,15 @@
 <main>
   <div class="content">
     <h1 style="text-align: center;">Meet Our Board</h1>
-    <div class="board-grid-container">
-      <!--<div class="member1">Member 1</div>
+    <!--<div class="board-grid-container">
+      <div class="member1">Member 1</div>
       <div class="member2">Member 2</div>
       <div class="member3">Member 3</div>
       <div class="member4">Member 4</div>
       <div class="member5">Member 5</div>
       <div class="member6">Member 6</div>
       <div class="member7">Member 7</div>-->
-    <div class="grid-container">
+    <div class="board-grid-container">
       <div class="item1">Header</div>
       <div class="item2">Menu</div>
       <div class="item3">Main</div>  
@@ -23,7 +23,7 @@
     </div>
 
 
-    </div>
+    <!--</div>-->
 
 
   </div>
---
+++ restore.php
@@ -0,0 +1,19 @@
+<?php include 'header.php';?>
+<?php include 'menu.php';?>
+
+<main class="content">
+    <h1 style="text-align: center;">ReStore</h1>
+    <p>
+        The ReStore works with volunteers and community residents to receive and resell 
+        donations that generate funds to help the affiliate fulfill their mission of 
+        improving neighborhoods and eliminating poverty housing in Dickinson County by 
+        building simple decent homes and selling those homes to eligible families.
+    </p>
+    <div class="restore-grid-container">
+      <div class="r1">Header</div>
+      <div class="r2">Menu</div>
+      <div class="r3">Main</div>  
+    </div>
+</main>
+
+<?php include 'footer.php';?>
