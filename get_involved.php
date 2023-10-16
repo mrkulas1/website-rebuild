7ddb7323424d084079d9ea5151d8133bdb1af16e
mrkulas1 <98844790+mrkulas1@users.noreply.github.com>
2023-10-16 13:22:37 -0700

    Get Involved and Newsletter Pages
    Created the page layout for the Get Involved and Newsletter pages.  Missing link functionality. Will add after each page has basic layout

--- get_involved.php
+++ get_involved.php
@@ -1,3 +1,60 @@
 <?php include 'header.php';?>
 <?php include 'menu.php';?>
+
+<main class="content">
+    <div class="get-involved">
+        <h1>Get Involved</h1>
+        <p>Volunteers are the backbone of Habitat. Every hour helps to build a home - whether on the construction
+            site, in the office, on a committee, raising funds, or working in the ReStore. There are many
+            ways for you to get involved and make a difference.</p>
+    </div>
+    <div class="flex-parent">
+        <div class="flex-child-element involved-img-1">
+            <img src="get_involved_images/Hakes ramp (2).jpg">
+            <h3>Working on a wheel chair ramp in Florence</h3>
+        </div>
+        <div class="flex-child-element involved-img-2">
+            <img src="get_involved_images/Shutter crew.jpg">
+            <h3>Painting Shutters in Vulcan</h3>
+        </div>
+    </div>
+    <div class="get-involved" style="margin-top: 5em;">
+        <h1>Volunteering - Not just for the Young</h1>
+        <p>We know how daunting a task it is to start up something new as the years tick by, so our friends over
+            at <a href="https://www.aginginplace.org" target="_blank">aginginplace.org</a> have created a wonderful guide to help ease 
+            the idea and help get you out helping your community.</p>
+
+        <p>Click the button below to learn more about how you can begin volunteering and giving back as a senior.</p>
+        <button type="button" class="senior-button">Volunteering as a Senior</button>
+    </div>
+    <div class="get-involved" style="margin-top: 3em;">
+        <h1>Helpful Links and Forms</h1>
+        <table class="vol-links-table">
+            <tr>
+                <td>Volunteering Information</td>
+                <td>Download</td>
+            </tr>
+            <tr>
+                <td>Youth Volunteering Information</td>
+                <td>Download</td>
+            </tr>
+            <tr>
+                <td>Volunteer Questionnaire</td>
+                <td>Download</td>
+            </tr>
+            <tr>
+                <td>Youth Volunteer Questionnaire</td>
+                <td>Download</td>
+            </tr>
+            <tr>
+                <td>Volunteer Waiver</td>
+                <td>Download</td>
+            </tr>
+            <tr>
+                <td>Youth Volunteer Waiver</td>
+                <td>Download</td>
+            </tr>
+        </table>
+    </div>
+</main>
 <?php include 'footer.php';?>
--- our_services.php
+++ our_services.php
@@ -1,3 +1,7 @@
 <?php include 'header.php';?>
 <?php include 'menu.php';?>
+<main class="content">
+
+</main>
 <?php include 'footer.php';?>
+<?php include 'closing.php';?>
