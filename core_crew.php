bfe089d6bcf7bd130626b95daa29b04c8979b9c9
mrkulas1 <98844790+mrkulas1@users.noreply.github.com>
2023-10-17 15:57:21 -0700

    Core Crew and Shop Pages
    Both need images and style updates to make it look better, but the information is correct

---
+++ core_crew.php
@@ -0,0 +1,18 @@
+<?php include 'header.php';?>
+<?php include 'menu.php';?>
+<main class="content">
+    <div>
+        <h1>Core Crew</h1><br>
+        <p>We have a dedicated crew of men and women who build the majority of our homes. They work Monday's and Wednesday's
+            8:00am - Noon every week throughout the year. There's no experience necessary to join this crew. We even supply
+            tools.</p><br>
+        <p>The core crew is involved with everything.  They form and pour basements, raise walls, set trusses, place windows,
+            run wires, hang drywall and cabinets, and install the trim. You can become a full time member or just help when
+            you can. This important crew is our most valuable asset.</p><br>
+        <p>To volunteer or receive more information about joining this critical group of men and women, please contact the
+            HFHMR office at (906) 779-5377 or hfh@habitatmr.com.</p>
+
+    </div>
+</main>
+<?php include 'footer.php';?>
+<?php include 'closing.php';?>
---
+++ shop.php
@@ -0,0 +1,102 @@
+<?php include 'header.php';?>
+<?php include 'menu.php';?>
+
+<main class="content">
+    <div>
+        <h1>Shopping at the ReStore</h1><br>
+        <p>The ReStore is a home improvement retail outlet for inexpensive building and remodeling materials.  Our inventory 
+            is constantly changing based on the donations we receive. You never know what you'll find at the ReStore! If 
+            you have questions regarding availability of specific items, please call (906) 774-2867.</p>
+        <h2>Delivery</h2><br>
+        <p>No way to get your purchases home? No problem. Our ReStore offers a delivery service.  Call now to ask for current
+            pricing.</p>
+    </div>
+    <div class="shop-flex-container">
+    <div class="shop-flex-object">
+        <ul>
+            <li>Appliances</li>
+            <ul>
+                <li>Dryers</li>
+                <li>Washers</li>
+                <li>Refrigerators</li>
+                <li>Freezers</li>
+                <li>Stoves</li>
+                <li>Microwaves</li>
+                <li>Small Appliances</li>
+            </ul>
+            <li>Building Materials</li>
+            <ul>
+                <li>Cabinets</li>
+                <li>Ceiling Tiles</li>
+                <li>Countertops</li>
+                <li>Laminate Flooring</li>
+                <li>Bricks</li>
+                <li>Plywood</li>
+                <li>Shingles</li>
+                <li>Tile Flooring</li>
+                <li>Lumber</li>
+            </ul>
+            <li>Doors</li>
+            <li>Electronical Supplies</li>
+            <li>Electronics</li>
+            <li>Antique Furniture</li>
+            <li>Bedroom Furniture</li>
+            <ul>
+                <li>Dressers</li>
+                <li>Headboards</li>
+            </ul>
+            <li>Dining Furniture</li>
+            <ul>
+                <li>Chairs</li>
+                <li>Tables</li>
+            </ul>
+            <li>Living Room Furniture</li>
+            <ul>
+                <li>Chairs</li>
+                <li>Coffee Tables</li>
+                <li>End Tables</li>
+                <li>Love Seats</li>
+                <li>Sofas</li>
+                <li>Mirrors</li>
+                <li>Entertainment Centers</li>
+            </ul>
+        </ul>
+    </div>
+        <div class="shop-flex-object">
+            <ul>
+                <li>Office Furniture</li>
+                <ul>
+                    <li>Desk Chairs</li>
+                    <li>Desks</li>
+                    <li>Filing Cabinets</li> 
+                </ul>
+                <li>Tools</li>
+                <li>Hardware</li>
+                <ul>
+                    <li>Nails</li>
+                    <li>Screws</li>
+                    <li>Nuts and Bolts</li>
+                </ul>
+                <li>Knick Knacks</li>
+                <li>Lighting</li>
+                <ul>
+                    <li>Bulbs</li>
+                    <li>Globes</li>
+                    <li>Fixtures</li>
+                </ul>
+                <li>Mattresses</li>
+                <li>Paint</li>
+                <li>Plumbing</li>
+                <ul>
+                    <li>Sinks</li>
+                    <li>Vanities</li>
+                </ul>
+                <li>Rugs</li>
+                <li>Windows</li>
+            </ul>
+        </div>
+    </div>
+</main>
+
+<?php include 'footer.php';?>
+<?php include 'closing.php';?>
