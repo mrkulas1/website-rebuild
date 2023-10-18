567840b94579ae78ecd5d1f2e526197ac52488d1
mrkulas1 <98844790+mrkulas1@users.noreply.github.com>
2023-10-18 12:05:28 -0700

    ReStore Volunteer page functional

---
+++ volunteer.php
@@ -0,0 +1,35 @@
+<?php include 'header.php';?>
+<?php include 'menu.php';?>
+<main class="content">
+    <div>
+        <h1>Volunteering in the ReStore</h1>
+        <p>As a non-profit, the Habitat for Humanity Menominee River ReStore is always looking for good volunteers to help with
+            daily operations.
+        </p><br>
+        <p>Contact the ReStore by calling (906) 774-2867 or by emailing <a href="mailto:restore@habitatmr.com">restore@habitatmr.com</a>
+            for more information or to volunteer.</p>
+    </div>
+    <div class="re-vol-flex-con">
+        <div class="re-vol-flex-obj">
+            <h2>Mechanically Inclined?</h2>
+            <p>If you love to take items apart and put them back together again, we would love to have you help us. Many of
+                our donated items come in needing a little TLC, and this is a great way to volunteer time.
+            </p>
+        </div>
+        <div class="re-vol-flex-obj">
+            <h2>Love to clean?</h2>
+            <p>Many of our donated items would be benefitted by a good cleaning, and would be a perfect opportunity to get 
+                involved with the ReStore.
+            </p>
+        </div>
+        <div class="re-vol-flex-obj">
+            <h2>Love to organize?</h2>
+            <p>Because we never know what or how many items are coming in each day, there is always a need to continue
+                organizing and rearranging our showroom. If you have a passion for organizing things and would like to
+                see the store look orderly, we would love to have you donate your time.
+            </p>
+        </div>
+    </div>
+</main>
+<?php include 'footer.php';?>
+<?php include 'closing.php';?>
