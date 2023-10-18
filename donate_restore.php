00b5c7dd40941c7f6942d7c4afc617df1df09f54
mrkulas1 <98844790+mrkulas1@users.noreply.github.com>
2023-10-17 17:03:56 -0700

    Donate ReStore page functional
    Needs better styling

---
+++ donate_restore.php
@@ -0,0 +1,29 @@
+<?php include 'header.php';?>
+<?php include 'menu.php';?>
+<main class="content">
+    <div>
+        <h1>Donate to the ReStore</h1>
+        <p>Our ReStore accepts donations of appliances, furniture, building materials, household goods and more from
+            individuals or companies. <strong>Donations can be new or used, but all items must be in good repair and 
+            in saleable condition.</strong> To check if your donation is acceptable, please call the ReStore at 
+            (906) 774-2867. ReStore staff will make the final decision on all donated items.</p>
+        <p>At this time, we will not accept donations of <strong>box springs, mattresses, TV's or computer equipment.</strong></p>
+        <p>There are two ways to donate, by dropping off items to a ReStore employee at our donation dock, or by 
+            calling and scheduling a pickup.</p>
+    </div>
+    <div class="re-don-flex-con">
+        <div class="re-don-flex-obj">
+            <h1>Drop Offs</h1>
+            <p>Donations can be dropped off at the store's donation dock from 10-3 Tuesday through Friday, and 
+                normal business hours Saturday.</p>
+        </div>
+        <div class="re-don-flex-obj">
+            <h1>Pick Ups</h1>
+            <p>If the donation is large or you can't drop it off at the store we offer free pickups. Items must be on the
+                ground level outside the house in an easily accessible position. Pick-up's are done on Tuesdays and Thursdays
+                from 9-1. To schedule a pick up please call (906) 774-2867.</p>
+        </div>
+    </div>
+</main>
+<?php include 'footer.php';?>
+<?php include 'closing.php';?>
