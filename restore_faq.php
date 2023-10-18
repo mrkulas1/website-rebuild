43a53cf9ad91a39d050d96d7d00e10e03930fa27
mrkulas1 <98844790+mrkulas1@users.noreply.github.com>
2023-10-18 16:34:14 -0700

    Most of ReStore FAQs
    Need to add in some specific IRS statements and links

---
+++ restore_faq.php
@@ -0,0 +1,34 @@
+<?php include 'header.php';?>
+<?php include 'menu.php';?>
+<main class="content">
+    <div>
+        <h1>ReStore FAQs</h1>
+        <h2>Are all the items for sale in the store donated?</h2>
+        <p>No. While most of our items are donated, we do have items that have been purchased by us or are put up by
+            employees as consignment.
+        </p>
+        <h2>If I buy a big item, can you deliver it for me?</h2>
+        <p>No. The ReStore no longer offers a delivery service to customers with larger items. It is the customer's 
+            responisibility to load and transport all items they purchase.
+        </p>
+        <h2>I have items to donate, do you have anyone who can pick them up?</h2>
+        <p>Yes. Please call the ReStore to make an appointment.</p>
+        <h2>Do you take TVs?</h2>
+        <p>We do not take older TVs. If it is a flat screen in good working condition, we may accept it at our discretion.</p>
+        <h2>Can I take a tax deduction for my donations?</h2>
+        <p>If you itemize deductions on your federal tax return, you may be entitled to claim a charitable deduction for
+            your ReStore donations. According to the Internal Revenue Service (IRS), a taxpayer can deduct the fair 
+            market value of household goods, used furniture, books and so forth. Fair market value is the price a 
+            willing buyer would pay for them. Value usually depends on the condition of the item. By law, a charity cannot
+            tell you what your donated items are worth. This is something you must do yourself. To assess "fair market value"
+            for your donations:
+        </p>
+        <ul>
+            <li>Consult a local tax advisor who should be familiar with market values in your region</li>
+            <li>Review the following tax guides available from the IRS</li>
+        </ul>
+        
+    </div>
+</main>
+<?php include 'footer.php';?>
+<?php include 'closing.php';?>
