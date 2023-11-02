0978b798c6f9d09dc447af6a88ee411f59f076df
mrkulas1 <98844790+mrkulas1@users.noreply.github.com>
2023-11-02 10:21:07 -0700

    lazy loading for house photos

--- house_photos.php
+++ house_photos.php
@@ -8,51 +8,51 @@
         <h1>House Photos</h1>
     </div>
     <div class="photo-grid">
-        <div class="grid-element"><img src="house_images/2 Balsam-Jenson.jpg"></div>
-        <div class="grid-element"><img src="house_images/6 Breen-Vasquez.jpg"></div>
-        <div class="grid-element"><img src="house_images/7 Breen-Wolf.jpg"></div>
-        <div class="grid-element"><img src="house_images/8 Breen-LaValley.jpg"></div>
-        <div class="grid-element"><img src="house_images/9 Roseland-Osborne.jpg"></div>
-        <div class="grid-element"><img src="house_images/10 Quincy-Hiltonen.jpg"></div>
-        <div class="grid-element"><img src="house_images/11 Henford - Heikkinen.jpg"></div>
-        <div class="grid-element"><img src="house_images/12 Menominee-Lidbeck.jpg"></div>
-        <div class="grid-element"><img src="house_images/13 Mullen Krieg.jpg"></div>
-        <div class="grid-element"><img src="house_images/14 Wells-Wonch.jpg"></div>
-        <div class="grid-element"><img src="house_images/15 Central-Warra.jpg"></div>
-        <div class="grid-element"><img src="house_images/16 Central-Degenaer.jpg"></div>
-        <div class="grid-element"><img src="house_images/17 Marpe-Hudson.jpg"></div>
-        <div class="grid-element"><img src="house_images/18 7th St.-Fisher.jpg"></div>
-        <div class="grid-element"><img src="house_images/19 5th St.-Zellner.jpg"></div>
-        <div class="grid-element"><img src="house_images/20 Aragon-Peterson.jpg"></div>
-        <div class="grid-element"><img src="house_images/21-Main - Starnes.jpg"></div>
-        <div class="grid-element"><img src="house_images/22 Main Peterson.jpg"></div>
-        <div class="grid-element"><img src="house_images/23 Parkway-Baker.jpg"></div>
-        <div class="grid-element"><img src="house_images/24 Vulcan Collins.jpg"></div>
-        <div class="grid-element"><img src="house_images/25 Cayuga-Nowicki.jpg"></div>
-        <div class="grid-element"><img src="house_images/26 Mullen-Langston.jpg"></div>
-        <div class="grid-element"><img src="house_images/27 Brown-Hirth.jpg"></div>
-        <div class="grid-element"><img src="house_images/28 W.A.-Jokinen.jpg"></div>
-        <div class="grid-element"><img src="house_images/29 Sagola-Ozzello.jpg"></div>
-        <div class="grid-element"><img src="house_images/30 Pisoni-Hamel.jpg"></div>
-        <div class="grid-element"><img src="house_images/31 Hemlock-Nanninga.jpg"></div>
-        <div class="grid-element"><img src="house_images/32 Central Olsen.jpg"></div>
-        <div class="grid-element"><img src="house_images/33 Schuirmann Cty Rd N.jpg"></div>
-        <div class="grid-element"><img src="house_images/34 Withworth Eichinger.jpg"></div>
-        <div class="grid-element"><img src="house_images/35 Central-Opolka.jpg"></div>
-        <div class="grid-element"><img src="house_images/36 Central-Haferkorn.jpg"></div>
-        <div class="grid-element"><img src="house_images/37 Adams Conibear.jpg"></div>
-        <div class="grid-element"><img src="house_images/38 Withworth Kamin.jpg"></div>
-        <div class="grid-element"><img src="house_images/39 Smith Hudson.jpg"></div>
-        <div class="grid-element"><img src="house_images/40 Coolidge Fleury.jpg"></div>
-        <div class="grid-element"><img src="house_images/41 Cayuga Christianson.jpg"></div>
-        <div class="grid-element"><img src="house_images/42 West D Goad.jpg"></div>
-        <div class="grid-element"><img src="house_images/43 West D Glaser.jpg"></div>
-        <div class="grid-element"><img src="house_images/44 West B.jpg"></div>
-        <div class="grid-element"><img src="house_images/45 Coolidge-Roberts.jpg"></div>
-        <div class="grid-element"><img src="house_images/46 Balkum.jpg"></div>
-        <div class="grid-element"><img src="house_images/47 W656.jpeg"></div>
-        <div class="grid-element"><img src="house_images/49 1005 Adams done.jpg"></div>
-        <div class="grid-element"><img src="house_images/50 139 W Lincoln.jpg"></div>
+        <div class="grid-element"><img src="house_images/2 Balsam-Jenson.jpg" loading="lazy"></div>
+        <div class="grid-element"><img src="house_images/6 Breen-Vasquez.jpg" loading="lazy"></div>
+        <div class="grid-element"><img src="house_images/7 Breen-Wolf.jpg" loading="lazy"></div>
+        <div class="grid-element"><img src="house_images/8 Breen-LaValley.jpg" loading="lazy"></div>
+        <div class="grid-element"><img src="house_images/9 Roseland-Osborne.jpg" loading="lazy"></div>
+        <div class="grid-element"><img src="house_images/10 Quincy-Hiltonen.jpg" loading="lazy"></div>
+        <div class="grid-element"><img src="house_images/11 Henford - Heikkinen.jpg" loading="lazy"></div>
+        <div class="grid-element"><img src="house_images/12 Menominee-Lidbeck.jpg" loading="lazy"></div>
+        <div class="grid-element"><img src="house_images/13 Mullen Krieg.jpg" loading="lazy"></div>
+        <div class="grid-element"><img src="house_images/14 Wells-Wonch.jpg" loading="lazy"></div>
+        <div class="grid-element"><img src="house_images/15 Central-Warra.jpg" loading="lazy"></div>
+        <div class="grid-element"><img src="house_images/16 Central-Degenaer.jpg" loading="lazy"></div>
+        <div class="grid-element"><img src="house_images/17 Marpe-Hudson.jpg" loading="lazy"></div>
+        <div class="grid-element"><img src="house_images/18 7th St.-Fisher.jpg" loading="lazy"></div>
+        <div class="grid-element"><img src="house_images/19 5th St.-Zellner.jpg" loading="lazy"></div>
+        <div class="grid-element"><img src="house_images/20 Aragon-Peterson.jpg" loading="lazy"></div>
+        <div class="grid-element"><img src="house_images/21-Main - Starnes.jpg" loading="lazy"></div>
+        <div class="grid-element"><img src="house_images/22 Main Peterson.jpg" loading="lazy"></div>
+        <div class="grid-element"><img src="house_images/23 Parkway-Baker.jpg" loading="lazy"></div>
+        <div class="grid-element"><img src="house_images/24 Vulcan Collins.jpg" loading="lazy"></div>
+        <div class="grid-element"><img src="house_images/25 Cayuga-Nowicki.jpg" loading="lazy"></div>
+        <div class="grid-element"><img src="house_images/26 Mullen-Langston.jpg" loading="lazy"></div>
+        <div class="grid-element"><img src="house_images/27 Brown-Hirth.jpg" loading="lazy"></div>
+        <div class="grid-element"><img src="house_images/28 W.A.-Jokinen.jpg" loading="lazy"></div>
+        <div class="grid-element"><img src="house_images/29 Sagola-Ozzello.jpg" loading="lazy"></div>
+        <div class="grid-element"><img src="house_images/30 Pisoni-Hamel.jpg" loading="lazy"></div>
+        <div class="grid-element"><img src="house_images/31 Hemlock-Nanninga.jpg" loading="lazy"></div>
+        <div class="grid-element"><img src="house_images/32 Central Olsen.jpg" loading="lazy"></div>
+        <div class="grid-element"><img src="house_images/33 Schuirmann Cty Rd N.jpg" loading="lazy"></div>
+        <div class="grid-element"><img src="house_images/34 Withworth Eichinger.jpg" loading="lazy"></div>
+        <div class="grid-element"><img src="house_images/35 Central-Opolka.jpg" loading="lazy"></div>
+        <div class="grid-element"><img src="house_images/36 Central-Haferkorn.jpg" loading="lazy"></div>
+        <div class="grid-element"><img src="house_images/37 Adams Conibear.jpg" loading="lazy"></div>
+        <div class="grid-element"><img src="house_images/38 Withworth Kamin.jpg" loading="lazy"></div>
+        <div class="grid-element"><img src="house_images/39 Smith Hudson.jpg" loading="lazy"></div>
+        <div class="grid-element"><img src="house_images/40 Coolidge Fleury.jpg" loading="lazy"></div>
+        <div class="grid-element"><img src="house_images/41 Cayuga Christianson.jpg" loading="lazy"></div>
+        <div class="grid-element"><img src="house_images/42 West D Goad.jpg" loading="lazy"></div>
+        <div class="grid-element"><img src="house_images/43 West D Glaser.jpg" loading="lazy"></div>
+        <div class="grid-element"><img src="house_images/44 West B.jpg" loading="lazy"></div>
+        <div class="grid-element"><img src="house_images/45 Coolidge-Roberts.jpg" loading="lazy"></div>
+        <div class="grid-element"><img src="house_images/46 Balkum.jpg" loading="lazy"></div>
+        <div class="grid-element"><img src="house_images/47 W656.jpeg" loading="lazy"></div>
+        <div class="grid-element"><img src="house_images/49 1005 Adams done.jpg" loading="lazy"></div>
+        <div class="grid-element"><img src="house_images/50 139 W Lincoln.jpg" loading="lazy"></div>
     </div>
 </main>
 
