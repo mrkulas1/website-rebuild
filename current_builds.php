89cb1c713b5af2c789e7849fe52017b0853bbad2
mrkulas1 <98844790+mrkulas1@users.noreply.github.com>
2024-01-10 14:28:48 -0800

    Added current build image
    Added 726 house with siding

--- current_builds.php
+++ current_builds.php
@@ -6,7 +6,8 @@
 <main class="content">
     <div>
         <h1>Current Builds</h1>
-        
+        <img src="about_us_images/726 Front Partial Siding.jpg">
+        <p>Construction is finishing up at our newest house in Norway, Michigan.</p>
     </div>
 </main>
 
